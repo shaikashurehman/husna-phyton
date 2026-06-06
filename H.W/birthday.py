d40bb8a284a7f8516d1eecb92d755bac5d866238
shaikashurehman <shaikashurehman@gmail.com>
2026-06-06 16:50:56 +0300

    birthdayhomework

---
+++ H.W/birthday.py
@@ -0,0 +1,11 @@
+Abdul="7 September"
+Ashu="25 December"
+Husna="29July"
+Misbah="1 December"
+Aathika="7January"
+name=input("enter ur name")
+print("Abdul Rahiman's birthday is on",Abdul)
+print("Ashu's birthday is on",Ashu)
+print("Husna'sbirthday is on",Husna)
+print("Misbah's birthday is on",Misbah)
+print("Aathika's birthday is on",Aathika)
