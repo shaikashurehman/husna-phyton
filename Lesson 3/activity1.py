9b398d5f0d95cf112206321957b9ecbc0e6334e6
shaikashurehman <shaikashurehman@gmail.com>
2026-06-06 17:20:55 +0300

    Lesson3

---
+++ Lesson 3/activity1.py
@@ -0,0 +1,23 @@
+ #1) Store an integer value in `a`.
+
+# 2) Print the datatype of `a` using `type()`.
+
+#example:
+
+a = 5
+
+print("type of a: ", type(a))
+
+# 3) Store a decimal (float) value in `b`.
+b=10.5
+# 4) Print the datatype of `b` using `type()`.
+print("type of b:",type(b))
+# 5) Store a text (string) value in `c`.
+c="husna"
+# 6) Print the datatype of `c` using `type()`.
+print("type of c:",type(c))
+# 7) Store a boolean value (True/False) in `d`.
+d=True
+# 8) Print the datatype of `d` using `type()`.
+print("type of d:",type(d))
+                        
---
+++ Lesson 3/activity2.py
@@ -0,0 +1,35 @@
+# 1) Create variables to store different types of values:
+
+# - `name` as text (string)
+
+name = "Penguin"
+
+# - `age` as a whole number (integer)
+age=20
+# - `is_student` as True/False (boolean)
+
+# - `weight` as a decimal number (float)
+weight=40.5
+# 2) Print each variable’s value.
+
+# 3) Print the datatype of each variable using `type()`.
+
+print("Name :", name)
+
+print("Data Type of Name is", type(name))
+
+# 4) Show a message that type casting will happen next.
+
+# 5) Convert `age` from an integer to a string and store it back in `age`.
+
+age = str(age)
+
+print(age)
+
+# 6) Print `age` and print its datatype again to confirm it changed.
+
+# 7) Convert `weight` from a float to an integer and store it back in `weight`.
+weight=int(weight)
+# 8) Print `weight` and print its datatype again to confirm it changed.
+
+print("Data Type of weight is", type(weight),"and the new weight is ",weight)
