9a5a7f92c476f706626e8cce59ce8ae94108a601
shaikashurehman <shaikashurehman@gmail.com>
2026-06-14 16:40:37 +0300

    lesson8

---
+++ lesson 8/activity1.py
@@ -0,0 +1,35 @@
+# 1) Store values in `v`, `w`, `x`, `y`, and `z`.
+
+# 2) Calculate the expression (v + w) * x / y and store the result back in `z`.
+
+# 3) Print the value of `z` with a message.
+v=4
+w=5
+x=8
+y=2
+z=0
+z=(v + w) * x / y 
+print("the value of z becomes",z)
+
+# 4) Store a name in `name` and a number in `age`.
+
+#Remember: and has higher precedence
+
+#if (name == "Alex") or ((name == "John") and (age >= 2)):
+
+# 5) Check this condition using `or` and `and`:
+
+# - The code checks if `name` is "Alex"
+
+# OR (if `name` is "John" AND `age` is 2 or more).
+
+# - If the condition is true, print the welcome message.
+
+# - Otherwise, print the goodbye message.
+name="Alex"
+age=0
+if (name == "Alex") or ((name == "John") and (age >= 2)):
+    print("hello welcome")
+else:
+    print("goodbye")
+  
---
+++ lesson 8/activity2.py
@@ -0,0 +1,6 @@
+num=4
+den=2
+if(num%den==0):
+    print("num is divisble by den compeletly")
+else:
+    print("not divisble")
